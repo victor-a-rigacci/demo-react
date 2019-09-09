2cc344c32e09217712a8c398dbceb58f82d15e7d
Victor Rigacci <victor.rigacci@gmail.com>
2019-09-08 22:20:49 -0300

    Updates

---
+++ precache-manifest.2c34fcd0bba16a20030b0e22d64e3ac0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4f1121e9f425350f9718c9794ccc399",
+    "url": "/demo-react/index.html"
+  },
+  {
+    "revision": "8468b09f2eac0759434c",
+    "url": "/demo-react/static/css/main.9e4c05a6.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/demo-react/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "8468b09f2eac0759434c",
+    "url": "/demo-react/static/js/main.971736b4.chunk.js"
+  },
+  {
+    "revision": "a5e4bab58b3c68a704c0",
+    "url": "/demo-react/static/js/runtime~main.d7c91bd5.js"
+  }
+]);
